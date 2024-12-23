993867cf607d7b9b9843b790df88360b4f1b68ad
vlad <ecscalibur_10@mail.ru>
2024-12-23 20:19:29 +0300

    NN for recognizing digits with no frameworks

--- smo.py
+++ smo.py
@@ -20,36 +20,86 @@ for i in range(1, N):
     client_time.append(client_time[i-1] + clients[i])
 client_time = np.array(client_time)
 
-time_1 = 0
-time_2 = 0
 
-T0 = T1 = T2 = 0
+def smo_no_queue():
+    T0 = T1 = T2 = 0
 
-for i in range(N):
-    if client_time[i] >= time_1 and client_time[i] >= time_2:
-        T0 += client_time[i] - max(time_1, time_2)
-        T1 += max(time_1, time_2) - min(time_1, time_2)
-        time_1 = time_2 = client_time[i]
-        time_1 += proc_1[i]
+    time_1 = 0
+    time_2 = 0
 
-    elif time_1 >= client_time[i] >= time_2:
-        T1 += client_time[i] - time_2
-        time_2 = client_time[i] + proc_2[i]
-        T2 += min(time_1, time_2) - client_time[i]
+    for i in range(N):
+        if client_time[i] >= time_1 and client_time[i] >= time_2:
+            T0 += client_time[i] - max(time_1, time_2)
+            T1 += max(time_1, time_2) - min(time_1, time_2)
+            time_1 = time_2 = client_time[i]
+            time_1 += proc_1[i]
 
-    elif time_2 >= client_time[i] >= time_1:
-        T1 += client_time[i] - time_1
-        time_1 = client_time[i] + proc_1[i]
-        T2 += min(time_1, time_2) - client_time[i]
+        elif time_1 >= client_time[i] >= time_2:
+            T1 += client_time[i] - time_2
+            time_2 = client_time[i] + proc_2[i]
+            T2 += min(time_1, time_2) - client_time[i]
 
-T = sum(clients)
+        elif time_2 >= client_time[i] >= time_1:
+            T1 += client_time[i] - time_1
+            time_1 = client_time[i] + proc_1[i]
+            T2 += min(time_1, time_2) - client_time[i]
 
-print('----My----')
-print("P0 =", T0 / T)
-print("P1 =", T1 / T)
-print("P2 =", T2 / T)
+    T = sum(clients)
 
-print('----Erlang----')
-print("P0 =", 1 / sum(p ** i / factorial(i) for i in range(3)))
-print("P1 =", p / sum(p ** i / factorial(i) for i in range(3)))
-print("P2 =", p * p / 2 / sum(p ** i / factorial(i) for i in range(3)))
+    print('----No Queue----')
+    print("P0 =", T0 / T)
+    print("P1 =", T1 / T)
+    print("P2 =", T2 / T)
+
+
+def smo_queue():
+    T0 = T1 = T2 = 0
+
+    time_1 = 0
+    time_2 = 0
+
+    for i in range(N):
+        if client_time[i] >= time_1 and client_time[i] >= time_2:
+            T0 += client_time[i] - max(time_1, time_2)
+            T1 += max(time_1, time_2) - min(time_1, time_2)
+            time_1 = time_2 = client_time[i]
+            time_1 += proc_1[i]
+
+        elif time_1 >= client_time[i] >= time_2:
+            T1 += client_time[i] - time_2
+            time_2 = client_time[i] + proc_2[i]
+            T2 += min(time_1, time_2) - client_time[i]
+
+        elif time_2 >= client_time[i] >= time_1:
+            T1 += client_time[i] - time_1
+            time_1 = client_time[i] + proc_1[i]
+            T2 += min(time_1, time_2) - client_time[i]
+
+        else:
+            if time_1 > time_2:
+                T2 += min(time_1, time_2 + proc_2[i]) - time_2
+                time_2 += proc_2[i]
+            else:
+                T2 += min(time_2, time_1 + proc_1[i]) - time_1
+                time_1 += proc_1[i]
+
+    T = sum(clients)
+
+    print('----Queue----')
+    print("P0 =", T0 / T)
+    print("P1 =", T1 / T)
+    print("P2 =", T2 / T)
+
+
+def erlang():
+    print('----Erlang----')
+    print("P0 =", 1 / sum(p ** i / factorial(i) for i in range(3)))
+    print("P1 =", p / sum(p ** i / factorial(i) for i in range(3)))
+    print("P2 =", p * p / 2 / sum(p ** i / factorial(i) for i in range(3)))
+
+
+smo_no_queue()
+print()
+smo_queue()
+print()
+erlang()
